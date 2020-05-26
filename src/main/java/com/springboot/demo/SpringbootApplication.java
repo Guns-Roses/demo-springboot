fc35ba25d96700bd675fe429cd416ed5007c5fcd
lvxutao <809283678@qq.com>
2020-05-26 14:30:53 +0800

    将服务注册到consul

--- src/main/java/com/springboot/demo/SpringbootApplication.java
+++ src/main/java/com/springboot/demo/SpringbootApplication.java
@@ -2,10 +2,14 @@ package com.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
+
+//支持服务发现
+@EnableDiscoveryClient
 public class SpringbootApplication {
 
     public static void main(String[] args) {
